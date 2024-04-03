0be80146908c57014045b4fd32a3280660a1fb57
Daniel Dahme <dahme@qualitus.de>
2024-04-03 13:53:30 +0200

    plugin class added

---
+++ src/DebuggerPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Debugger;
+
+use Api\IPlugin;
+use Api\ICheck;
+
+class DebuggerPlugin implements IPlugin, ICheck {
+
+	private $servicelocator;
+
+	public function __construct() {
+		$this->servicelocator = \Base3\ServiceLocator::getInstance();
+	}
+
+	// Implementation of IBase
+
+	public function getName() {
+		return "debuggerplugin";
+	}
+
+	// Implementation of IPlugin
+
+	public function init() {
+
+		$this->servicelocator
+			->set($this->getName(), $this, true)
+			;
+
+	}
+
+	// Implementation of ICheck
+
+	public function checkDependencies() {
+		return array(
+			"freetemplateplugin_installed" => $this->servicelocator->get('freetemplateplugin') ? "Ok" : "moduledpageplugin not installed"
+		);
+	}
+
+}
+
